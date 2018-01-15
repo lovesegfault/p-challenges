8a395479f7d9a690043a1d3cfc81d8ebfd6cff60
Bernardo Meurer <meurerbernardo@gmail.com>
2018-01-14 21:40:08 -0800

    Finished 00-name-generator
    Signed-off-by: Bernardo Meurer <meurerbernardo@gmail.com>

--- 00-name-generator/person-generator.c
+++ 00-name-generator/person-generator.c
@@ -1,4 +1,4 @@
-#include "name-generator.h"
+#include "person-generator.h"
 
 #define STREET_LIST  "lists/street_names.txt"
 #define FIRST_NAME_LIST  "lists/first_names.txt"
@@ -57,7 +57,7 @@ char *generate_SSN() {
     } while ((lead_number == 666) || (lead_number > 900));
 
     snprintf(SSN, 10, "%03zu%02zu%04zu",
-            lead_number, group_number, serial_number);
+             lead_number, group_number, serial_number);
 
     static const char *know_ad = "219099999";
     static const char *woolworth = "219099999";
@@ -163,8 +163,10 @@ char *generate_address() {
     unsigned int pick = generate_int(1, count_lines(name_list));
     char *name = get_line(name_list, pick);
 
+    fclose(name_list);
+
     const char *suf = suffixes[generate_int(0, 7)];
-    size_t sz = (strlen(name) + strlen(suf) + 2);
+    size_t sz = (strlen(name) + strlen(suf) + 3 + 4);
     char *street = calloc(sz, sizeof(char));
     strncpy(street, suf, sz * sizeof(char));
 
@@ -172,7 +174,14 @@ char *generate_address() {
     strcat(street, name);
 
     free(name);
-    fclose(name_list);
+
+    char *number = calloc(4, sizeof(char));
+    sprintf(number, "%d", generate_int(1, 9999));
+
+    strcat(street, " ");
+    strcat(street, number);
+
+    free(number);
     return street;
 }
 
@@ -180,12 +189,12 @@ char *generate_name() {
     FILE *first_names = fopen(FIRST_NAME_LIST, "r");
     FILE *last_names = fopen(LAST_NAME_LIST, "r");
 
-    char* first = get_line(first_names, generate_int(1, count_lines(first_names)));
-    char* last = get_line(last_names, generate_int(1, count_lines(last_names)));
+    char *first = get_line(first_names, generate_int(1, count_lines(first_names)));
+    char *last = get_line(last_names, generate_int(1, count_lines(last_names)));
 
     size_t sz = strlen(first) + strlen(last) + 2;
 
-    char *name = (char*)calloc(sz, sizeof(char));
+    char *name = (char *) calloc(sz, sizeof(char));
     strncpy(name, first, sz * sizeof(char));
 
     strcat(name, " ");
@@ -199,26 +208,132 @@ char *generate_name() {
 }
 
 char *minimize_str(char *str) {
-    char *out = (char*) calloc(strlen(str), sizeof(char));
-    for(int i = 0; i < strlen(str); ++i){
+    char *out = (char *) calloc(strlen(str), sizeof(char));
+    for (int i = 0; i < strlen(str); ++i) {
         out[i] = (char) tolower(str[i]);
     }
     return out;
 }
 
-char* generate_email(char* name) {
-    char *track;
-   // char *names[];
-    track = strtok(name, " ");
+
+/*
+ * split_str - Splits a string on delimiters
+ * @param str Input string to be split
+ * @param delim Delimiter to split at
+ * @param dest Destination pointer of tokens
+ * @return Number of tokens in dest
+ */
+size_t split_str(char *str, const char delim, char ***dest) {
+    if (str == NULL) return NULL;
+
+    size_t delim_count = 0;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == delim) {
+            ++delim_count;
+        }
+    }
+    char **tokens = calloc(delim_count + 1, sizeof(char *));
+    if (tokens == NULL) return NULL;
+
+    char *dup = strdup(str);
+    if (dup == NULL) return NULL;
+
+    char *found;
+    delim_count = 0;
+    while ((found = strsep(&dup, &delim)) != NULL) {
+        tokens[delim_count] = calloc(strlen(found) + 1, sizeof(char));
+        strcpy(tokens[delim_count], found);
+        ++delim_count;
+    }
+    free(dup);
+    *dest = tokens;
+
+    return delim_count;
 }
 
-person generate_person() {
-    person p = {};
-    p.address = generate_address();
-    p.DOB = generate_DOB();
-    //p.email
-    p.name = generate_name();
-    p.SSN = generate_SSN();
+void shuffle(char **arr, size_t n) {
+    if (n > 1) {
+        size_t i;
+        for (i = n - 1; i > 0; --i) {
+            size_t pick = generate_int(0, (unsigned int) n - 1);
+            char *save = arr[pick];
+            arr[pick] = arr[i];
+            arr[i] = save;
+        }
+    }
+}
 
-    return p;
+char *generate_email(char *name, struct tm DOB) {
+    bool use_dob = (bool) (random_uint() & 1);
+    bool use_dash = (bool) (random_uint() & 1);
+
+    const char *providers[3] = {"@gmail.com", "@hotmail.com", "@yahoo.com"};
+
+    char *provider = (char *) providers[generate_int(0, 2)];
+
+    char **tokens;
+    size_t name_count = split_str(name, ' ', &tokens);
+
+    size_t email_size = 0;
+    for (int i = 0; i < name_count; ++i) {
+        email_size += strlen(tokens[i]);
+    }
+    email_size += strlen(provider);
+    if (use_dash) {
+        email_size += name_count - 1;
+    }
+
+    char *year = NULL;
+    if (use_dob) {
+        year = calloc(3, sizeof(char));
+        strftime(year, 3, "%y", &DOB);
+        email_size += strlen(year);
+    }
+
+    char *email = calloc(email_size, sizeof(char));
+
+    shuffle(tokens, name_count);
+
+    for (int i = 0; i < name_count; ++i) {
+        strcat(email, minimize_str(tokens[i]));
+        if ((use_dash && i < name_count - 1) || (use_dash && use_dob)) {
+            strcat(email, "-");
+        }
+    }
+    if (use_dob) {
+        strcat(email, year);
+    }
+    strcat(email, provider);
+
+    return email;
+}
+
+void generate_person(person *self) {
+    self->address = generate_address();
+    self->DOB = generate_DOB();
+    self->name = generate_name();
+    self->email = generate_email(self->name, self->DOB);
+    self->SSN = generate_SSN();
+
+    self->print = person_print;
+    self->kill = person_kill;
+}
+
+void person_print(person *self) {
+    printf("SSN: %s\n", self->SSN);
+    char *dob = calloc(11, sizeof(char));
+    strftime(dob, 11, "%F", &(self->DOB));
+    printf("DOB: %s\n", dob);
+    free(dob);
+    printf("Address: %s\n", self->address);
+    printf("Name: %s\n", self->name);
+    printf("Email: %s\n", self->email);
+
+}
+
+void person_kill(person *self) {
+    free(self->address);
+    free(self->SSN);
+    free(self->name);
+    free(self->email);
 }
--- 00-name-generator/person-generator.h
+++ 00-name-generator/person-generator.h
@@ -9,12 +9,16 @@
 #include <time.h>
 #include <limits.h>
 
-typedef struct {
+typedef struct _person {
     char *name;
     char *email;
     char *address;
     char *SSN;
     struct tm DOB;
+
+    void (*print)(struct _person *);
+
+    void (*kill)(struct _person *);
 } person;
 
 char *generate_SSN();
@@ -24,5 +28,14 @@ struct tm generate_DOB();
 char *generate_address();
 
 char *generate_name();
+
+char *generate_email(char *name, struct tm DOB);
+
+void generate_person(person *);
+
+void person_print(person *);
+
+void person_kill(person *);
+
 
 #endif //INC_00_NAME_GENERATOR_NAME_GENERATOR_H
