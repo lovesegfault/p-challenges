b63263afa9b85f2436495e28a695e1f565a45a1f
Bernardo Meurer <meurerbernardo@gmail.com>
2018-01-19 00:21:08 -0800

    Removed old folders

--- 00-name-generator/main.c
+++
@@ -1,9 +0,0 @@
-#include "person-generator.h"
-
-int main() {
-    person p;
-    generate_person(&p);
-    p.print(&p);
-    p.kill(&p);
-    return 0;
-}
--- 01-heads-tails/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "heads-tails.h"
-
-int main() {
-    game();
-    return 0;
-}
