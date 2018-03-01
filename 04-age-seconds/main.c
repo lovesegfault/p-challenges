39fd8bb3ba3da94a2ffba6bf69d4f7870ced94a4
Bernardo Meurer <meurerbernardo@gmail.com>
2018-03-01 07:44:25 -0800

    Finished implementation

--- 04-age-seconds/main.c
+++ 04-age-seconds/main.c
@@ -1,6 +1,27 @@
+#define _XOPEN_SOURCE
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char *input = calloc(10, sizeof(char));
+    struct tm birthday ={};
+    while(true) {
+        printf("Please input your birthday in format YYYY-MM-DD: ");
+        if (fgets(input, 10, stdin) != NULL) {
+            if(*strptime(input, "%F", &birthday) == '\0') {
+                break;
+            } else {
+                continue;
+            }
+        } else {
+            continue;
+        }
+    }
+    time_t age = time(NULL) - mktime(&birthday);
+    printf("You are %zu seconds old!\n", age);
+    free(input);
     return 0;
 }
